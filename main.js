5d17a89093c56a13b111fc1bc6f211bbfd5484e1
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-03-18 10:46:07 -0600

    añadido pacienteAsegurado

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import Cita from "./cita.js"
-import Doctor from "./doctor.js"
-import Fecha from "./fecha.js"
-import Hospital from "./hospital.js"
-import Nombre from "./nombre.js"
-import Paciente from "./paciente.js"
-import Tiempo from "./tiempo.js"
+import Cita from "./cita.js";
+import Doctor from "./doctor.js";
+import Fecha from "./fecha.js";
+import Hospital from "./hospital.js";
+import Nombre from "./nombre.js";
+import Paciente from "./paciente.js";
+import Tiempo from "./tiempo.js";
+import PacienteAsegurado from "./pacienteAsegurado.js";
 
 class Main {
   constructor() {
